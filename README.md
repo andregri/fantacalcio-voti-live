ccab5a49f722b8e5a027aeadd03e6ca74b4c9c20
andregri <andrea.grillo96@live.com>
2022-02-05 19:29:06 +0100

    add cli arguments

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+python3 main.py --squadra "Milan" --giornata 24 --until "20:00"
+```
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import squadre
 import requests
 import schedule
+import argparse
 
 def request_voti_live(giornata, codice_squadra, magic_number):
     url = f"https://www.fantacalcio.it/api/live/{codice_squadra}?g={giornata}&i={magic_number}"
@@ -15,21 +16,44 @@ def request_voti_live(giornata, codice_squadra, magic_number):
     return json_resp_time
 
 
-def save_json(voti_live):
-    with open(f"resp_{voti_live['timestamp']}.json", "w") as f:
+def save_json(filename, voti_live):
+    with open(filename, "w") as f:
         json.dump(voti_live, f, indent=4)
 
 
-def task():
+def cli_args():
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s",
+        description="Record voti live"
+    )
+
+    parser.add_argument("--squadra", dest="squadra", type=str)
+    parser.add_argument("--giornata", dest="giornata", type=int)
+    parser.add_argument("--magic", dest="magic", type=int, default=16)
+    parser.add_argument("--until", dest="until", type=str)
+
+    return parser.parse_args()
+
+
+def task(giornata, squadra, magic):
     print(time.localtime())
-    voti_milan = request_voti_live(giornata=24, codice_squadra=squadre.codici["Milan"], magic_number=16)
-    save_json(voti_milan)
+    
+    voti = request_voti_live(giornata=giornata, codice_squadra=squadre.codici[squadra], magic_number=magic)
+
+    filename = f"{squadra}_{giornata}_{voti['timestamp']}.json"
+    save_json(filename, voti)
 
 
 if __name__ == "__main__":
-    print(time.localtime())
-    
-    schedule.every(30).seconds.do(task)
+    args = cli_args()
+    print(f"Squadra: {args.squadra}")
+    print(f"Giornata: {args.giornata}")
+    print(f"Until: {args.until}")
+
+    print(f"Start @ {time.localtime()}")
+
+    schedule.every(30).seconds.until(args.until).do(
+        task, args.giornata, args.squadra, args.magic)
 
     while True:
         schedule.run_pending()

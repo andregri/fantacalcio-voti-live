30d30bb75aa27d5b18c7a87e415cb55c4d9b5816
andregri <andrea.grillo96@live.com>
2022-02-05 18:59:11 +0100

    add timestamp and save to file

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+import json
+import time
 import squadre
-
 import requests
+import schedule
 
-def get_voti_live(giornata, codice_squadra, magic_number):
+def request_voti_live(giornata, codice_squadra, magic_number):
     url = f"https://www.fantacalcio.it/api/live/{codice_squadra}?g={giornata}&i={magic_number}"
     print(url)
-    voti_live = requests.get(url)
-    return voti_live.json()
+    response = requests.get(url, timeout=10)
+    json_resp = response.json()
+    json_resp_time = {
+        "voti": json_resp,
+        "timestamp": round(time.time())
+    }
+    return json_resp_time
 
 
-# Voti live genoa
-voti_roma = get_voti_live(giornata=24, codice_squadra=squadre.codici["Roma"], magic_number=16)
-print(voti_roma)
+def save_json(voti_live):
+    with open(f"resp_{voti_live['timestamp']}.json", "w") as f:
+        json.dump(voti_live, f, indent=4)
 
-voti_genoa = get_voti_live(giornata=24, codice_squadra=squadre.codici["Genoa"], magic_number=16)
-print(voti_genoa)
 
-voti_inter = get_voti_live(giornata=24, codice_squadra=squadre.codici["Inter"], magic_number=16)
-print(voti_inter)
-
-voti_milan = get_voti_live(giornata=24, codice_squadra=squadre.codici["Milan"], magic_number=16)
-print(voti_milan)
+if __name__ == "__main__":
+    voti_milan = request_voti_live(giornata=24, codice_squadra=squadre.codici["Milan"], magic_number=16)
+    save_json(voti_milan)
+    #schedule.every(3).seconds.do(request_voti_live, giornata=24, codice_squadra=squadre.codici["Inter"], magic_number=16)
+#
+    #while True:
+    #    schedule.run_pending()
+    #    time.sleep(1)

542004b60be40d5d1cc218868bbd65cfa037ab52
andregri <andrea.grillo96@live.com>
2022-02-05 19:06:27 +0100

    run task periodically

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import schedule
 
 def request_voti_live(giornata, codice_squadra, magic_number):
     url = f"https://www.fantacalcio.it/api/live/{codice_squadra}?g={giornata}&i={magic_number}"
-    print(url)
     response = requests.get(url, timeout=10)
     json_resp = response.json()
     json_resp_time = {
@@ -21,11 +20,17 @@ def save_json(voti_live):
         json.dump(voti_live, f, indent=4)
 
 
-if __name__ == "__main__":
+def task():
+    print(time.localtime())
     voti_milan = request_voti_live(giornata=24, codice_squadra=squadre.codici["Milan"], magic_number=16)
     save_json(voti_milan)
-    #schedule.every(3).seconds.do(request_voti_live, giornata=24, codice_squadra=squadre.codici["Inter"], magic_number=16)
-#
-    #while True:
-    #    schedule.run_pending()
-    #    time.sleep(1)
+
+
+if __name__ == "__main__":
+    print(time.localtime())
+    
+    schedule.every(30).seconds.do(task)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)

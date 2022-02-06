77a0f4e573370327266cc0223b0ba95aa29fc92f
andregri <andrea.grillo96@live.com>
2022-02-06 22:36:26 +0100

    add graph to dashboard that updates every 30s

--- dashboard.py
+++ dashboard.py
@@ -2,20 +2,13 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
+from sqlalchemy import create_engine, text
+import db as app_db
 
 app = Dash(__name__)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -24,11 +17,32 @@ app.layout = html.Div(children=[
         Dash: A web application framework for your data.
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+    dcc.Graph(id='live-update-graph'),
+
+    dcc.Interval(
+        id='interval-component',
+        interval=30*1000, # in milliseconds
+        n_intervals=0
     )
 ])
+
+
+@app.callback(Output('live-update-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_graph_live(n):
+    t = text("""
+        SELECT voto, timestamp FROM 
+            public.giocatore, 
+            public.voto
+        WHERE 
+            giocatore.id = voto.id_giocatore AND
+            giocatore.nome = :nome
+    """)
+    db_engine = create_engine(app_db.db_string)
+    voti_df = pd.read_sql(t, db_engine, params={'nome': 'BARAK'})
+    fig = px.line(voti_df, x="timestamp", y="voto")
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
